99170291e1687edb19e54844577a2bd0f6881c83
Guilherme Paulino <guilherme.paulino@msn.com>
2022-04-14 11:47:56 -0300

    Experimental results with Release compilation

--- experiments/ativ-3-exp-1/plot.py
+++ experiments/ativ-3-exp-1/plot.py
@@ -11,7 +11,7 @@ sns.set(color_codes=True)
 def user_input():
     """Parse the user input"""
     if(len(sys.argv) != 2):
-        print('Please, inform the CSV input file.')
+        print("Please, inform the CSV input file.")
         sys.exit(1)
     return sys.argv[1]
 
@@ -21,24 +21,26 @@ def prefix(file_path):
 
 def histogram(file_path, name=""):
     """Plot Histogram"""
-    values = np.genfromtxt(file_path, delimiter='\n')
+    values = np.genfromtxt(file_path, delimiter="\n")
     values = Series(values, name="Time of Execution (s)")
-    sns.distplot(values, hist=True)
-    plt.savefig(name + '_histogram.png')
+    sns.histplot(data=values, kde=True)
+    plt.savefig("images/" + name + "_histogram.png")
+    # plt.show()
 
 def linear_regression(y_axis, name=""):
     """Plot Linear Regression"""
     x_axis = np.arange(1, len(y_axis)+1)
-    data_merge = {'Iteration': x_axis, 'Time of Execution (s)': y_axis}
+    data_merge = {"Iteration": x_axis, "Time of Execution (s)": y_axis}
     data_frame = DataFrame(data=data_merge)
-    sns.lmplot(x='Iteration', y='Time of Execution (s)',
+    sns.lmplot(x="Iteration", y="Time of Execution (s)",
                data=data_frame, ci=95)
-    plt.savefig(name + '_linear_regression.png')
+    plt.savefig("images/" + name + "_linear_regression.png")
+    # plt.show()
 
 if __name__ == "__main__":
     try:
         file_path = user_input()
-        y_axis = np.genfromtxt(file_path, delimiter='\n')
+        y_axis = np.genfromtxt(file_path, delimiter="\n")
         file_prefix = prefix(file_path)
         histogram(file_path, file_prefix)
         linear_regression(y_axis, file_prefix)
